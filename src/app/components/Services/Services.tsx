55ea3c842d930cacd5e82b0611d753b58d57502a
Chris Ware <c.p.ware@hotmail.com>
2023-08-16 01:39:11 -0700

    Services Section Update

--- src/app/components/Services/Services.tsx
+++ src/app/components/Services/Services.tsx
@@ -2,6 +2,7 @@ import Label from "../Label/Label";
 import styles from "./Services.module.css";
 import { servicesSection } from "../../../lib/data";
 import Image from "next/image";
+import Button from "../Button/Button";
 
 const Services = () => {
   return (
@@ -10,21 +11,27 @@ const Services = () => {
         <div className={styles.top}>
           <Label text='Services' />
           <h2 className={styles.heading}>
-            Our Range of Web Development
-            Services
+            Our Range of Web Development Services
           </h2>
           <p className={styles.copy}>
             Feel free to contact us for custom soultions.
           </p>
         </div>
         <div className={styles.bottom}>
-          {servicesSection.map((x) => (
+          {servicesSection.slice(0, 1).map((x) => (
             <div key={x.id}>
               <h3 className={styles.heading2}>{x.sectionTitle}</h3>
               <div className={styles.serviceCardContainer}>
                 {x.services.map((y) => (
                   <div key={y.id} className={styles.serviceCard}>
-                    <Image src={y.icon} alt='hello' title="hello" width={50} height={50} className={styles.icon} />
+                    <Image
+                      src={y.icon}
+                      alt='hello'
+                      title='hello'
+                      width={50}
+                      height={50}
+                      className={styles.icon}
+                    />
                     <h4 className={styles.serviceTitle}>{y.service}</h4>
                     <p className={styles.description}>{y.description}</p>
                   </div>
@@ -32,6 +39,9 @@ const Services = () => {
               </div>
             </div>
           ))}
+        </div>
+        <div className={styles.btnContainer}>
+          <Button href='/services' text='More services' btnType='primary' />
         </div>
       </div>
     </section>
