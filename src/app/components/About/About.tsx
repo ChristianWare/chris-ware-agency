b1226cf7abac2a1dd69ae437665ee5eb729dbfaa
Chris Ware <c.p.ware@hotmail.com>
2023-08-16 02:20:23 -0700

    Button Border Update

--- src/app/components/About/About.tsx
+++ src/app/components/About/About.tsx
@@ -5,11 +5,21 @@ const About = () => {
   return (
     <section className={styles.container}>
       <div className={styles.content}>
-        <SectionHeading
-          labelText='About us'
-          headingText='Welcome to the Chris Ware Agency'
-          copyText='An overview of who we are'
-        />
+        <div className={styles.top}>
+          <SectionHeading
+            labelText='About us'
+            headingText='Welcome to the Chris Ware Agency'
+            copyText='An overview of who we are'
+          />
+        </div>
+        <div className={styles.bottom1}>
+          <div className={styles.left}>Image Here</div>
+          <div className={styles.right}>Text Here</div>
+        </div>
+        <div className={styles.bottom2}>
+          <div className={styles.left}>Text Here</div>
+          <div className={styles.right}>Image Here</div>
+        </div>
       </div>
     </section>
   );
