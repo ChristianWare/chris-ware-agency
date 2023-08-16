a64de5219f5bbcddf465ec41e1652ff22baeb641
Chris Ware <c.p.ware@hotmail.com>
2023-08-15 19:41:30 -0700

    Label FZ Update

--- src/app/components/Services/Services.tsx
+++ src/app/components/Services/Services.tsx
@@ -1,15 +1,15 @@
-import Button from "../Button/Button";
 import Label from "../Label/Label";
 import styles from "./Services.module.css";
 
-
 const Services = () => {
   return (
-    <section>
-      <Label text='Services' />
-      <h2>
-        Web Solutions Beyond Boundaries: Our Range of Web Development Services
-      </h2>
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <Label text='Services' />
+        <h2>
+          Web Solutions Beyond Boundaries: Our Range of Web Development Services
+        </h2>
+      </div>
     </section>
   );
 };
