e87f4c90eb2d37786abb9c2cfe8392d67f52fbf7
Chris Ware <c.p.ware@hotmail.com>
2023-08-16 03:23:36 -0700

    About Section Start

--- src/app/components/About/About.tsx
+++ src/app/components/About/About.tsx
@@ -1,5 +1,7 @@
 import SectionHeading from "../SectionHeading/SectionHeading";
 import styles from "./About.module.css";
+import Image from "next/image";
+import Chris from "../../../../public/images/chrisware.jpg";
 
 const About = () => {
   return (
@@ -9,16 +11,38 @@ const About = () => {
           <SectionHeading
             labelText='About us'
             headingText='Welcome to the Chris Ware Agency'
-            copyText='An overview of who we are'
+            copyText='An overview of who we are as a company and why you will love us!'
           />
         </div>
-        <div className={styles.bottom1}>
-          <div className={styles.left}>Image Here</div>
-          <div className={styles.right}>Text Here</div>
-        </div>
-        <div className={styles.bottom2}>
-          <div className={styles.left}>Text Here</div>
-          <div className={styles.right}>Image Here</div>
+        <div className={styles.bottom}>
+          <div className={styles.left}>
+            <div className={styles.imgContainer}>
+              <Image
+                src={Chris}
+                alt='hello'
+                className={styles.img}
+                fill
+                sizes='100vw'
+              />
+            </div>
+          </div>
+          <div className={styles.right}>
+            <h3>About ya Boy</h3>
+            <p>
+              I&rsquo;m Chris Ware, the owner and creative force behind The
+              Chris Ware Agency. My journey in the world of web development
+              began during my college years, where I discovered my passion for
+              crafting digital experiences. Over the past decade and a half,
+              I&&rsquo;ve honed my skills and transformed that passion into a
+              thriving career. As the driving force behind The Chris Ware
+              Agency, I am dedicated to combining cutting-edge technologies with
+              innovative design to build websites that not only meet my
+              clients&rsquo; needs but also exceed their expectations. With a
+              deep-rooted commitment to continuous learning and a keen eye for
+              detail, I am excited to bring your web projects to life and create
+              digital solutions that leave a lasting impact.
+            </p>
+          </div>
         </div>
       </div>
     </section>
