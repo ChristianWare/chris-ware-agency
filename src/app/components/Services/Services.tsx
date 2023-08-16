7cd543d140587e832143edafd9315b38389f33d5
Chris Ware <c.p.ware@hotmail.com>
2023-08-16 00:13:08 -0700

    New PNG Files

--- src/app/components/Services/Services.tsx
+++ src/app/components/Services/Services.tsx
@@ -1,5 +1,7 @@
 import Label from "../Label/Label";
 import styles from "./Services.module.css";
+import { servicesSection } from "../../../lib/data";
+import Image from "next/image";
 
 const Services = () => {
   return (
@@ -9,11 +11,28 @@ const Services = () => {
           <Label text='Services' />
           <h2 className={styles.heading}>
             Web Solutions Beyond Boundaries: Our Range of Web Development
-            Offerings
+            Services
           </h2>
+          <p className={styles.copy}>
+            Feel free to contact us for custom soultions.
+          </p>
         </div>
         <div className={styles.bottom}>
-
+          {/* <h3 className={styles.heading2}>What We Build</h3> */}
+          {servicesSection.map((x) => (
+            <div key={x.id}>
+              {x.sectionTitle}
+              <div>
+                {x.services.map((y) => (
+                  <div key={y.id}>
+                    <h4>{y.service}</h4>
+                    <p>{y.description}</p>
+                    <Image src={y.icon} alt='hello' width={50} height={50} />
+                  </div>
+                ))}
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </section>
---
+++ src/lib/data.ts
@@ -0,0 +1,103 @@
+import Webdesign from "../../public/icons/webdesign.png";
+import Analytics from "../../public/icons/an.png";
+import Landing from "../../public/icons/landing.png";
+import Market from "../../public/icons/market.png";
+import Research from "../../public/icons/research.png";
+import SEO from "../../public/icons/seo.png";
+import Fe from "../../public/icons/fe.png";
+import Be from "../../public/icons/be.png";
+import Bolt from "../../public/icons/bolt.png";
+import Blog from "../../public/icons/blog.png";
+
+
+
+export const servicesSection = [
+  {
+    id: 1,
+    sectionTitle: "What We Build",
+    services: [
+      {
+        id: 1.1,
+        icon: Market,
+        service: "Marketing Websites",
+        description:
+          "We build you dynamic and engaging marketing websites that not only showcase your brand but also drive results. ",
+      },
+      {
+        id: 1.2,
+        icon: Webdesign,
+        service: "E-Commerce",
+        description:
+          "Empower your online business with our expertly crafted E-Commerce websites designed for seamless shopping experiences and maximum conversion.",
+      },
+      {
+        id: 1.3,
+        icon: Landing,
+        service: "Landing Pages",
+        description:
+          "Elevate your campaign's impact with our precision-engineered landing page sites optimized for conversion and engagement.",
+      },
+      {
+        id: 1.4,
+        icon: Blog,
+        service: "Blogs",
+        description:
+          "Unlock your voice and reach with our bespoke blog sites, tailored for captivating content delivery and audience connection.",
+      },
+    ],
+  },
+  {
+    id: 2,
+    sectionTitle: "SEO Services",
+    services: [
+      {
+        id: 2.1,
+        icon: SEO,
+        service: "SEO Audits",
+        description:
+          "Uncover your website's full potential with our comprehensive SEO audits, revealing actionable insights for enhanced online visibility.",
+      },
+      {
+        id: 2.2,
+        icon: Research,
+        service: "Keyword Research",
+        description:
+          "Fuel your online success with our strategic keyword research, driving targeted traffic and boosting search rankings.",
+      },
+      {
+        id: 2.3,
+        icon: Analytics,
+        service: "Analytics and Reporting",
+        description:
+          "Transform your data into insights with our website analytics and reporting services, guiding informed decisions for digital growth.",
+      },
+    ],
+  },
+  {
+    id: 3,
+    sectionTitle: "Development Services",
+    services: [
+      {
+        id: 3.1,
+        icon: Be,
+        service: "Backend Development",
+        description:
+          "Power your website with robust functionality and performance through our expert backend development solutions.",
+      },
+      {
+        id: 3.2,
+        icon: Fe,
+        service: "Frontend Development",
+        description:
+          "Elevate user experiences and design with our cutting-edge frontend development services, creating visually stunning and intuitive websites.",
+      },
+      {
+        id: 3.3,
+        icon: Bolt,
+        service: "Page Speed Optimization",
+        description:
+          "Accelerate your online success with our page speed optimization services, ensuring faster loading times and superior user experiences.",
+      },
+    ],
+  },
+] as const;
