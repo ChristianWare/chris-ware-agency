08b6fbf3568539b222b6391079f28b887963f193
Chris Ware <c.p.ware@hotmail.com>
2023-08-20 14:26:59 -0700

    FAQ Functionality Working

--- src/app/components/Faq/Faq.tsx
+++ src/app/components/Faq/Faq.tsx
@@ -1,7 +1,21 @@
+"use client";
+
+import { useState } from "react";
 import SectionHeading from "../SectionHeading/SectionHeading";
 import styles from "./Faq.module.css";
+import { faqs } from "@/lib/data";
 
 const Faq = () => {
+  const [selected, setSelected] = useState<null | number>(null);
+
+  const toggle = (i: any) => {
+    if (selected === i) {
+      return setSelected(null);
+    }
+
+    setSelected(i);
+  };
+
   return (
     <section className={styles.container}>
       <div className={styles.content}>
@@ -10,6 +24,26 @@ const Faq = () => {
           headingText='Frequently Asked Questions'
           copyText='Common questions and answers'
         />
+        <div className={styles.bottom}>
+          {faqs.map((x, i) => (
+            <div
+              key={x.id}
+              className={styles.qaContainer}
+              onClick={() => toggle(i)}
+            >
+              <h3 className={styles.question}>{x.question}</h3>
+              <div
+                className={
+                  selected === i
+                    ? styles.answerContainer + " " + styles.show
+                    : styles.answerContainer
+                }
+              >
+                <p className={styles.answer}>{x.answer}</p>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </section>
   );
