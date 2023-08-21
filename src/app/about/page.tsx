faa4c7b164bb3f1ee731ee4af2c21e44bd6baa62
Chris Ware <c.p.ware@hotmail.com>
2023-08-21 14:20:24 -0700

    About Page Done

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,8 @@
 import CompanyCulture from "../components/CompanyCulture/CompanyCulture";
+import ContactSection from "../components/ContactSection/ContactSection";
+import Faq from "../components/Faq/Faq";
 import PageIntro from "../components/PageIntro/PageIntro";
+import Process from "../components/Process/Process";
 import Reasons from "../components/Reasons/Reasons";
 import Team from "../components/Team/Team";
 import styles from "./About.module.css";
@@ -7,16 +10,18 @@ import styles from "./About.module.css";
 const AboutPage = () => {
   return (
     <section className={styles.container}>
-      {/* <div className={styles.pageIntroContainer}>
+      <div className={styles.pageIntroContainer}>
         <PageIntro
           label='About Us'
           heading='About the Chris Ware Agency'
           copy='Our journey is rooted in a passion for crafting captivating websites that transcend mere functionality. We embark on a mission to transform ideas into pixel-perfect realities. From sleek and responsive designs to seamless user interactions, we infuse every project with a blend of cutting-edge technology and artistic flair. Join us as we unfold our story, driven by a commitment to redefine the digital landscape and empower businesses through unparalleled web solutions.'
         />
-      </div> */}
-      {/* <Reasons /> */}
-      {/* <Team /> */}
+      </div>
+      <Reasons />
       <CompanyCulture />
+      <Team />
+      <Faq />
+      <ContactSection />
     </section>
   );
 };
--- src/app/components/CompanyCulture/CompanyCulture.tsx
+++ src/app/components/CompanyCulture/CompanyCulture.tsx
@@ -2,6 +2,9 @@ import styles from "./CompanyCulture.module.css";
 import Section from "../Section/Section";
 import Image from "next/image";
 import img from "../../../../public/images/affordable.png";
+import img2 from "../../../../public/images/collab1.png";
+import img3 from "../../../../public/images/collab2.png";
+import img4 from "../../../../public/images/collab3.png";
 
 const CompanyCulture = () => {
   return (
@@ -10,12 +13,12 @@ const CompanyCulture = () => {
       headingText='work culture & philosophy'
       copyText='what it&#39;s like to work with us'
     >
-      <div className={styles.workCulture}>
-        <div className={styles.content}>
+      <div className={styles.content}>
+        <div className={styles.workCulture}>
           <div className={styles.card}>
             <div className={styles.imgContainer}>
               <Image
-                src={img}
+                src={img3}
                 alt='hey'
                 title='hey'
                 fill
@@ -32,20 +35,18 @@ const CompanyCulture = () => {
             </p>
           </div>
           <div className={styles.card3}>
-            <div className={styles.imgContainer}>
-              <Image
-                src={img}
-                alt='hey'
-                title='hey'
-                fill
-                className={styles.img3}
-              />
-            </div>
+            <h3 className={styles.heading}>Client-Centric Dedication</h3>
+            <p className={styles.copy}>
+              Our culture revolves around serving our clients with dedication
+              and excellence. We go the extra mile to understand their goals,
+              communicate effectively, and ensure that our web development
+              solutions align with their vision and business objectives.
+            </p>
           </div>
           <div className={styles.card4}>
             <div className={styles.imgContainer}>
               <Image
-                src={img}
+                src={img4}
                 alt='hey'
                 title='hey'
                 fill
@@ -54,6 +55,26 @@ const CompanyCulture = () => {
             </div>
           </div>
         </div>
+        {/* <div className={styles.bottom}>
+          <div className={styles.left}>
+            <h3>Client-Centric Dedication</h3>
+            <p className={styles.copy}>
+              Our culture revolves around serving our clients with dedication
+              and excellence. We go the extra mile to understand their goals,
+              communicate effectively, and ensure that our web development
+              solutions align with their vision and business objectives.
+            </p>
+          </div>
+          <div className={styles.left}>
+            <h3>Adaptability and Agilit</h3>
+            <p className={styles.copy}>
+              In the dynamic world of web development, adaptability is key. Our
+              culture embraces change and agility. We&#39;re quick to adapt to
+              emerging technologies, industry trends, and client needs, ensuring
+              that we&#39;re always one step ahead.
+            </p>
+          </div>
+        </div> */}
       </div>
     </Section>
   );
